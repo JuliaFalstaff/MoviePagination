5b08f3c2c735b4a766c6ec43db822c37ecf40775
Julia Kholstinina <j.kholstinina@yandex.ru>
2022-11-22 22:52:09 +0300

    add mapper and dto

--- app/src/main/java/com/example/moviepagination/utils/Mapper.kt
+++ app/src/main/java/com/example/moviepagination/utils/Mapper.kt
@@ -3,10 +3,10 @@ package com.example.moviepagination.utils
 import com.example.moviepagination.domain.entities.MovieItemList
 import com.example.moviepagination.data.network.model.MovieItemListDto
 
-fun convertFromDataToEntity(list: MovieItemList): MovieItemListDto {
-    return MovieItemListDto(id = 0, items = )
-}
-
-fun convertFromEntityToMovieList(list: MovieItemListDto): MovieItemList {
-    return MovieItemList(list.items)
-}
+//fun convertFromDataToEntity(list: MovieItemList): MovieItemListDto {
+//    return MovieItemListDto(id = 0, items = )
+//}
+//
+//fun convertFromEntityToMovieList(list: MovieItemListDto): MovieItemList {
+//    return MovieItemList(list.items)
+//}
