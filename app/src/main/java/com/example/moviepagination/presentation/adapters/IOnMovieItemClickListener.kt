918ac81805ec3c8fb0d1e4527058d39593cb1107
Julia Kholstinina <j.kholstinina@yandex.ru>
2022-11-30 19:10:44 +0300

    delete unnecessary interface

--- app/src/main/java/com/example/moviepagination/presentation/adapters/IOnMovieItemClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviepagination.presentation.adapters
-
-import com.example.moviepagination.domain.entities.castInfo.CastMovie
-import com.example.moviepagination.domain.entities.castInfo.KnownFor
-
-interface IOnMovieItemClickListener {
-    fun onItemClick(movie: CastMovie)
-    fun onItemKnownClick(movie: KnownFor)
-}
