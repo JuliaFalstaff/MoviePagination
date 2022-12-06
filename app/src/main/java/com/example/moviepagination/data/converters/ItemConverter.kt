f09bbf0bd94dcb7bf9cc47e2badd43d558986ab5
Julia Kholstinina <j.kholstinina@yandex.ru>
2022-12-06 19:11:53 +0300

    change type converter for itemdbmodel

--- app/src/main/java/com/example/moviepagination/data/converters/ItemConverter.kt
+++ app/src/main/java/com/example/moviepagination/data/converters/ItemConverter.kt
@@ -1,5 +1,22 @@
 package com.example.moviepagination.data.converters
 
+import androidx.room.TypeConverter
 import com.example.moviepagination.data.database.model.ItemDbModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-class ItemConverter : BaseConverter<ItemDbModel>()
+class ItemConverter {
+    @TypeConverter
+    fun mapListToString(value: List<ItemDbModel>): String {
+        val gson = Gson()
+        val type = object : TypeToken<List<ItemDbModel>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun mapStringToList(value: String): List<ItemDbModel> {
+        val gson = Gson()
+        val type = object : TypeToken<List<ItemDbModel>>() {}.type
+        return gson.fromJson(value, type)
+    }
+}
