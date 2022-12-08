8e0a637cb6162e6a7382924634b3d6a1983c4976
Julia Kholstinina <j.kholstinina@yandex.ru>
2022-12-08 18:24:44 +0300

    add warning of empty fav data

--- app/src/main/java/com/example/moviepagination/presentation/SavedMovieListFragment.kt
+++ app/src/main/java/com/example/moviepagination/presentation/SavedMovieListFragment.kt
@@ -53,6 +53,7 @@ class SavedMovieListFragment :
                 val movieList = state.dataMovie
                 movieListAdapter?.submitList(movieList)
                 binding.progressBar.visibility = View.INVISIBLE
+                if (movieList.isEmpty()) showEmptyDataWarning()
             }
             is AppState.Loading -> {
                 binding.progressBar.visibility = View.VISIBLE
@@ -66,5 +67,9 @@ class SavedMovieListFragment :
                 Log.d("TAG saved list", "${state.error.stackTrace.toString()}")
             }
         }
+    }
+
+    private fun showEmptyDataWarning() {
+        binding.warningNoDataTextView.visibility = View.VISIBLE
     }
 }
